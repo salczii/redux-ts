ea5981c53f5f6324fa9c4f524e2ad7139a2fb0b0
Kacper Salek <kacper.salek32@gmail.com>
2021-03-17 13:11:15 +0100

    use redux store and dispatch in my app

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,8 +1,28 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+import { useActions } from "../hooks/useActions";
+
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState("");
+  const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  );
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    searchRepositories(term);
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={term}
+          type="text"
+          onChange={(e) => setTerm(e.target.value)}
+        />
         <button>Search</button>
       </form>
     </div>
